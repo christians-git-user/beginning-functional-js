fb9f61bb14604fbac6cf422387c86071e4d42cfe
Christian Steckler <christian.steckler@gmail.com>
2022-04-04 13:19:00 -0400

    added currying file and README

---
+++ README.md
@@ -0,0 +1,7 @@
+# functional-js
+
+## Working through the following resources to master functional programming in JavaScript:
+- A. Aravinth, S. MachirajuBeginning Functional JavaScript
+
+## Other helpful resources
+- Videos on using ramda.js https://www.youtube.com/watch?v=AINnOyUVEyI
--- currying.js
+++ currying.js
@@ -1,5 +1,7 @@
 // chapter 6: currying and partial application
 
+// arity is the number of arguments the function takes
+
 // unary fn
 const identity = x => x;
 
@@ -13,5 +15,72 @@ const variadic = (a, ...variadic) => {
     console.log(a)
     console.log(variadic)
 }
+
+// currying
+
+// manually curry a function
+const addCurried = x => y => x + y;
+
+// curry any binary function (es5 format)
+const curryBinary = (binaryFunction) => {
+    return function(firstArg) {
+        return function(secondArg) {
+            return binaryFunction(firstArg, secondArg);
+        }
+    }
+}
+
+const autoCurriedAdd = curryBinary(add);
+
+// curry Function Converting n arg Function to Unary Function
+let curry =(fn) => {
+    if(typeof fn!=='function'){
+        throw Error('No function provided');
+    }
+    return function curriedFn(...args){
+      if(args.length < fn.length){
+        return function(){
+          return curriedFn.apply(null, args.concat( [].slice.call(arguments) ));
+        };
+      }
+      return fn.apply(null, args);
+    };
+};
+
+
+
+const multiply = (x,y,z) => x * y * z;
+
+// curry(multiply)(1)(2)(3) this works
+
+const multiply2 = (...args) => args.reduce((a,b) => a * b);
+// curry(multiply2)(1)(2)(3)(4)(5)(6)//(720) does not take enough arguments
+
+const multiplyAny = num1 => {
+    return (num2) => {
+        if(!num2) {
+            return num1;
+        }
+        return multiplyAny(num1 * num2);
+    }
+}
+
+// console.log(multiplyAny(1)(2)(3)(4)(5)()); // this is dope, but requires the empty invocation ()
+
+// logger function
+const loggerHelper = (mode,initialMessage,errorMessage,lineNo) => {
+    if(mode === "DEBUG")
+            console.debug(initialMessage,errorMessage + "at line: " + lineNo)
+    else if(mode === "ERROR")
+            console.error(initialMessage,errorMessage + "at line: " + lineNo)
+    else if(mode === "WARN")
+            console.warn(initialMessage,errorMessage + "at line: " + lineNo)
+    else
+            throw "Wrong mode"
+};
+
+
+
+
 
 
